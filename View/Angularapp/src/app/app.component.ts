8e8a6ae9d62d01d4858507d12eb682f924e89875
rabindra kumar sah <rabindrasah79@gmail.com>
2024-10-08 12:41:48 +0200

    create Angular App

---
+++ View/Angularapp/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ProfileComponent, FriendsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angularapp';
+}
